6749b215eaca1924508a5f62a47d6c3fa2c36ff9
Christian Gapp <gapp.christian@gmail.com>
2014-07-14 23:47:50 +0200

    Extracted algorithms to https://github.com/gappc/biohadoop-algorithms.git

--- scripts/docker-copy-files.sh
+++ scripts/docker-copy-files.sh
@@ -12,10 +12,10 @@ source $1
 
 LIB_TMP_DIR=/tmp/lib
 LIB_HDFS_DIR=/biohadoop/lib
-DATA_TMP_DIR=/tmp/data
-DATA_HDFS_DIR=/biohadoop/data
-CONF_TMP_DIR=/tmp/conf
-CONF_HDFS_DIR=/biohadoop/conf
+#DATA_TMP_DIR=/tmp/data
+#DATA_HDFS_DIR=/biohadoop/data
+#CONF_TMP_DIR=/tmp/conf
+#CONF_HDFS_DIR=/biohadoop/conf
 
 function build {
   $MVN_HOME/bin/mvn -f $BIOHADOOP_PROJECT_HOME clean install
@@ -39,35 +39,35 @@ function copyLibRemote {
   echo "Copying libs from remote FS to remote HDFS - OK"
 }
 
-function copyDataRemote {
-  echo "Copying data to remote FS"
-  ssh root@172.17.0.100 "mkdir -p $DATA_TMP_DIR"
-  ssh root@172.17.0.100 "rm $DATA_TMP_DIR/*"
-  scp -r $BIOHADOOP_PROJECT_HOME/data/* root@172.17.0.100:$DATA_TMP_DIR
-
-  echo "Copying data from remote FS to remote HDFS"
-  ssh root@172.17.0.100 "/opt/hadoop/current/bin/hdfs dfs -rm -r $DATA_HDFS_DIR"
-  ssh root@172.17.0.100 "/opt/hadoop/current/bin/hdfs dfs -mkdir -p $DATA_HDFS_DIR"
-  ssh root@172.17.0.100 "/opt/hadoop/current/bin/hdfs dfs -copyFromLocal -f $DATA_TMP_DIR/* $DATA_HDFS_DIR"
-  echo "Copying data from remote FS to remote HDFS - OK"
-}
-
-function copyConfRemote {
-  echo "Copying configs to remote FS"
-  ssh root@172.17.0.100 "mkdir -p $CONF_TMP_DIR"
-  ssh root@172.17.0.100 "rm $CONF_TMP_DIR/*"
-  scp -r $BIOHADOOP_PROJECT_HOME/conf/* root@172.17.0.100:$CONF_TMP_DIR
-
-  echo "Copying configs from remote FS to remote HDFS"
-  ssh root@172.17.0.100 "/opt/hadoop/current/bin/hdfs dfs -rm -r $CONF_HDFS_DIR"
-  ssh root@172.17.0.100 "/opt/hadoop/current/bin/hdfs dfs -mkdir -p $CONF_HDFS_DIR"
-  ssh root@172.17.0.100 "/opt/hadoop/current/bin/hdfs dfs -copyFromLocal -f $CONF_TMP_DIR/* $CONF_HDFS_DIR"
-  echo "Copying configs from remote FS to remote HDFS - OK"
-}
+#function copyDataRemote {
+#  echo "Copying data to remote FS"
+#  ssh root@172.17.0.100 "mkdir -p $DATA_TMP_DIR"
+#  ssh root@172.17.0.100 "rm $DATA_TMP_DIR/*"
+#  scp -r $BIOHADOOP_PROJECT_HOME/data/* root@172.17.0.100:$DATA_TMP_DIR
+#
+#  echo "Copying data from remote FS to remote HDFS"
+#  ssh root@172.17.0.100 "/opt/hadoop/current/bin/hdfs dfs -rm -r $DATA_HDFS_DIR"
+#  ssh root@172.17.0.100 "/opt/hadoop/current/bin/hdfs dfs -mkdir -p $DATA_HDFS_DIR"
+#  ssh root@172.17.0.100 "/opt/hadoop/current/bin/hdfs dfs -copyFromLocal -f $DATA_TMP_DIR/* $DATA_HDFS_DIR"
+#  echo "Copying data from remote FS to remote HDFS - OK"
+#}
+#
+#function copyConfRemote {
+#  echo "Copying configs to remote FS"
+#  ssh root@172.17.0.100 "mkdir -p $CONF_TMP_DIR"
+#  ssh root@172.17.0.100 "rm $CONF_TMP_DIR/*"
+#  scp -r $BIOHADOOP_PROJECT_HOME/conf/* root@172.17.0.100:$CONF_TMP_DIR
+#
+#  echo "Copying configs from remote FS to remote HDFS"
+#  ssh root@172.17.0.100 "/opt/hadoop/current/bin/hdfs dfs -rm -r $CONF_HDFS_DIR"
+#  ssh root@172.17.0.100 "/opt/hadoop/current/bin/hdfs dfs -mkdir -p $CONF_HDFS_DIR"
+#  ssh root@172.17.0.100 "/opt/hadoop/current/bin/hdfs dfs -copyFromLocal -f $CONF_TMP_DIR/* $CONF_HDFS_DIR"
+#  echo "Copying configs from remote FS to remote HDFS - OK"
+#}
 
 #### main part ####
 build
 copyDependencies
 copyLibRemote
-copyDataRemote
-copyConfRemote
+#copyDataRemote
+#copyConfRemote
