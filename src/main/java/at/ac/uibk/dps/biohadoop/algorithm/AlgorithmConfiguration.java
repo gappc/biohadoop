2f1c99150de043f8acfabcb3e8b6475b230ecb6e
Christian Gapp <gapp.christian@gmail.com>
2014-08-15 04:43:42 +0200

    Deleted unused interface

--- src/main/java/at/ac/uibk/dps/biohadoop/algorithm/AlgorithmConfiguration.java
+++
@@ -1,8 +0,0 @@
-package at.ac.uibk.dps.biohadoop.algorithm;
-
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
-@JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, include=JsonTypeInfo.As.PROPERTY, property="@algorithmConfigClass")
-public interface AlgorithmConfiguration {
-	
-}
