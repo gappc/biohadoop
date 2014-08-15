7301f7877b615579142ffbfece988c5f042e2b10
Christian Gapp <gapp.christian@gmail.com>
2014-08-15 04:10:05 +0200

    Algorithm interface changed

--- src/main/java/at/ac/uibk/dps/biohadoop/algorithm/Algorithm.java
+++ src/main/java/at/ac/uibk/dps/biohadoop/algorithm/Algorithm.java
@@ -1,9 +1,10 @@
 package at.ac.uibk.dps.biohadoop.algorithm;
 
+import at.ac.uibk.dps.biohadoop.solver.SolverConfiguration;
 import at.ac.uibk.dps.biohadoop.solver.SolverId;
 
-public interface Algorithm<T> {
+public interface Algorithm {
 
-	public void compute(SolverId solverId, T parameter) throws AlgorithmException;
+	public void compute(SolverId solverId, SolverConfiguration configuration) throws AlgorithmException;
 	
 }
