1ee9e0c7ea1d0e2ab102f3fe192345151a0370c8
Christian Gapp <gapp.christian@gmail.com>
2014-08-15 04:14:14 +0200

    Configuration changed

--- src/main/java/at/ac/uibk/dps/biohadoop/hadoop/BiohadoopConfiguration.java
+++ src/main/java/at/ac/uibk/dps/biohadoop/hadoop/BiohadoopConfiguration.java
@@ -1,10 +1,17 @@
 package at.ac.uibk.dps.biohadoop.hadoop;
 
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import at.ac.uibk.dps.biohadoop.communication.CommunicationConfiguration;
-import at.ac.uibk.dps.biohadoop.handler.distribution.ZooKeeperConfiguration;
+import at.ac.uibk.dps.biohadoop.communication.MasterConfiguration;
+import at.ac.uibk.dps.biohadoop.communication.RemoteExecutable;
+import at.ac.uibk.dps.biohadoop.communication.WorkerConfiguration;
+import at.ac.uibk.dps.biohadoop.communication.master.MasterEndpoint;
+import at.ac.uibk.dps.biohadoop.communication.worker.WorkerEndpoint;
 import at.ac.uibk.dps.biohadoop.solver.SolverConfiguration;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
@@ -13,23 +20,21 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class BiohadoopConfiguration {
 
 	public static final String CONFIG_PATH = "CONFIG_PATH";
-	
-	private final List<String> includePaths;
-	private final List<SolverConfiguration> solverConfiguration;
-	private final CommunicationConfiguration communicationConfiguration;
-	private final ZooKeeperConfiguration zooKeeperConfiguration;
-	private final Map<String, String> properties;
 
-	public BiohadoopConfiguration(List<String> includePaths,
-			List<SolverConfiguration> solverConfiguration,
+	private final List<String> includePaths;
+	private final List<SolverConfiguration> solverConfigurations;
+	private final CommunicationConfiguration communicationConfiguration;
+	private final Map<String, String> globalProperties;
+
+	public BiohadoopConfiguration(
+			List<String> includePaths,
+			List<SolverConfiguration> solverConfigurations,
 			CommunicationConfiguration communicationConfiguration,
-			ZooKeeperConfiguration zooKeeperConfiguration,
-			Map<String, String> properties) {
+			Map<String, String> globalProperties) {
 		this.includePaths = includePaths;
-		this.solverConfiguration = solverConfiguration;
+		this.solverConfigurations = solverConfigurations;
 		this.communicationConfiguration = communicationConfiguration;
-		this.zooKeeperConfiguration = zooKeeperConfiguration;
-		this.properties = properties;
+		this.globalProperties = globalProperties;
 	}
 
 	@JsonCreator
@@ -37,30 +42,80 @@ public class BiohadoopConfiguration {
 			@JsonProperty("includePaths") List<String> includePaths,
 			@JsonProperty("solverConfiguration") List<SolverConfiguration> solverConfiguration,
 			@JsonProperty("communicationConfiguration") CommunicationConfiguration communicationConfiguration,
-			@JsonProperty("zooKeeperConfiguration") ZooKeeperConfiguration zooKeeperConfiguration,
-			@JsonProperty("properties") Map<String, String> properties) {
+			@JsonProperty("globalProperties") Map<String, String> globalProperties) {
 		return new BiohadoopConfiguration(includePaths, solverConfiguration,
-				communicationConfiguration, zooKeeperConfiguration, properties);
+				communicationConfiguration, globalProperties);
 	}
 
 	public List<String> getIncludePaths() {
 		return includePaths;
 	}
 
-	public List<SolverConfiguration> getSolverConfiguration() {
-		return solverConfiguration;
+	public List<SolverConfiguration> getSolverConfigurations() {
+		return solverConfigurations;
 	}
 
 	public CommunicationConfiguration getCommunicationConfiguration() {
 		return communicationConfiguration;
 	}
 
-	public ZooKeeperConfiguration getZooKeeperConfiguration() {
-		return zooKeeperConfiguration;
+	public Map<String, String> getGlobalProperties() {
+		return globalProperties;
 	}
+	
+	public static class Builder {
+		private List<String> libPaths = new ArrayList<>();
+		private List<SolverConfiguration> solverConfigurations = new ArrayList<>();;
+		private List<MasterConfiguration> dedicatedMasters = new ArrayList<>();
+		private List<WorkerConfiguration> workerConfigurations = new ArrayList<>();
+		private Map<String, String> globalProperties = new HashMap<>();
 
-	public Map<String, String> getProperties() {
-		return properties;
+		public Builder addLibPath(String path) {
+			libPaths.add(path);
+			return this;
+		}
+
+		public Builder addSolver(SolverConfiguration solverConfiguration) {
+			solverConfigurations.add(solverConfiguration);
+			return this;
+		}
+		
+		public Builder addDedicatedMaster(
+				Class<? extends MasterEndpoint> dedicatedMaster,
+				Class<? extends RemoteExecutable<?, ?, ?>> remoteExecutable,
+				Class<? extends Annotation> annotation) {
+			MasterConfiguration masterConfiguration = new MasterConfiguration(
+					dedicatedMaster, remoteExecutable, annotation);
+			dedicatedMasters.add(masterConfiguration);
+			return this;
+		}
+		
+		public Builder addWorker(Class<? extends WorkerEndpoint> worker, int count) {
+			addDedicatedWorker(worker, null, count);
+			return this;
+		}
+
+		public Builder addDedicatedWorker(Class<? extends WorkerEndpoint> worker,
+				Class<? extends RemoteExecutable<?, ?, ?>> remoteExecutable,
+				int count) {
+			WorkerConfiguration workerConfiguration = new WorkerConfiguration(
+					worker, remoteExecutable, count);
+			workerConfigurations.add(workerConfiguration);
+			return this;
+		}
+		
+		public Builder addGobalProperty(String key, String value) {
+			globalProperties.put(key, value);
+			return this;
+		}
+
+		public BiohadoopConfiguration build() {
+			CommunicationConfiguration communicationConfiguration = new CommunicationConfiguration(
+					dedicatedMasters, workerConfigurations);
+			return new BiohadoopConfiguration(libPaths, solverConfigurations,
+					communicationConfiguration, globalProperties);
+		}
+
 	}
 
 }
--- src/main/java/at/ac/uibk/dps/biohadoop/solver/SolverConfiguration.java
+++ src/main/java/at/ac/uibk/dps/biohadoop/solver/SolverConfiguration.java
@@ -1,10 +1,9 @@
 package at.ac.uibk.dps.biohadoop.solver;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import at.ac.uibk.dps.biohadoop.algorithm.Algorithm;
-import at.ac.uibk.dps.biohadoop.algorithm.AlgorithmConfiguration;
-import at.ac.uibk.dps.biohadoop.handler.HandlerConfiguration;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -12,44 +11,54 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class SolverConfiguration {
 
 	private final String name;
-	private final AlgorithmConfiguration algorithmConfiguration;
-	private final Class<? extends Algorithm<?>> algorithm;
-	private final List<HandlerConfiguration> handlerConfigurations;
+	private final Class<? extends Algorithm> algorithm;
+	private final Map<String, String> properties;
 
 	// TODO check if builder pattern is better suited
 	public SolverConfiguration(String name,
-			AlgorithmConfiguration algorithmConfiguration,
-			Class<? extends Algorithm<?>> algorithm,
-			List<HandlerConfiguration> handlerConfigurations) {
+			Class<? extends Algorithm> algorithm,
+			Map<String, String> properties) {
 		this.name = name;
-		this.algorithmConfiguration = algorithmConfiguration;
 		this.algorithm = algorithm;
-		this.handlerConfigurations = handlerConfigurations;
+		this.properties = properties;
 	}
 
 	@JsonCreator
-	public static SolverConfiguration create(
-			@JsonProperty("name") String name,
-			@JsonProperty("algorithmConfiguration") AlgorithmConfiguration algorithmConfiguration,
-			@JsonProperty("algorithm") Class<? extends Algorithm<?>> algorithm,
-			@JsonProperty("handlerConfiguration") List<HandlerConfiguration> handlerConfigurations) {
-		return new SolverConfiguration(name, algorithmConfiguration,
-				algorithm, handlerConfigurations);
+	public static SolverConfiguration create(@JsonProperty("name") String name,
+			@JsonProperty("algorithm") Class<? extends Algorithm> algorithm,
+			@JsonProperty("properties") Map<String, String> properties) {
+		return new SolverConfiguration(name, algorithm, properties);
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public AlgorithmConfiguration getAlgorithmConfiguration() {
-		return algorithmConfiguration;
-	}
-
-	public Class<? extends Algorithm<?>> getAlgorithm() {
+	public Class<? extends Algorithm> getAlgorithm() {
 		return algorithm;
 	}
 
-	public List<HandlerConfiguration> getHandlerConfigurations() {
-		return handlerConfigurations;
+	public Map<String, String> getProperties() {
+		return properties;
+	}
+
+	public static class Builder {
+		private String algorithmName;
+		private Class<? extends Algorithm> algorithmClass;
+		private Map<String, String> properties = new HashMap<>();
+
+		public Builder(String algorithmName, Class<? extends Algorithm> algorithmClass) {
+			this.algorithmName = algorithmName;
+			this.algorithmClass = algorithmClass;
+		}
+
+		public Builder addProperty(String key, String value) {
+			properties.put(key, value);
+			return this;
+		}
+
+		public SolverConfiguration build() {
+			return new SolverConfiguration(algorithmName, algorithmClass, properties);
+		}
 	}
 }
