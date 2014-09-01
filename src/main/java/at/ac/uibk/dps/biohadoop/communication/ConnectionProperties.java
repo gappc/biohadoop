226146f78a7ddb0342719776926a0583d6f4d532
Christian Gapp <gapp.christian@gmail.com>
2014-09-01 21:25:59 +0200

    Connection timeouts set to 15 sec

--- src/main/java/at/ac/uibk/dps/biohadoop/communication/ConnectionProperties.java
+++ src/main/java/at/ac/uibk/dps/biohadoop/communication/ConnectionProperties.java
@@ -2,7 +2,7 @@ package at.ac.uibk.dps.biohadoop.communication;
 
 public class ConnectionProperties {
 
-	public static final int CONNECTION_TIMEOUT = 5000;
-	public static final int READ_TIMEOUT = 5000;
+	public static final int CONNECTION_TIMEOUT = 15000;
+	public static final int READ_TIMEOUT = 15000;
 
 }
