72ec6b1cdb16d9ed65a6a61676cb3e32d25ae676
Christian Gapp <gapp.christian@gmail.com>
2014-09-08 01:29:31 +0200

    Removed commented-out code

--- src/main/java/at/ac/uibk/dps/biohadoop/communication/Message.java
+++ src/main/java/at/ac/uibk/dps/biohadoop/communication/Message.java
@@ -9,7 +9,6 @@ public class Message<T> implements Serializable {
 	private static final long serialVersionUID = -6406631824326170469L;
 
 	private MessageType type;
-//	@JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, include=JsonTypeInfo.As.PROPERTY, property="@taskClass")
 	private Task<T> task;
 
 	public Message() {
