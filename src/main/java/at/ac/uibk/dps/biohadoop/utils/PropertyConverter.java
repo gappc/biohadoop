b732a7652960cbb39662e57b858f8b9f9d3a9e62
Christian Gapp <gapp.christian@gmail.com>
2014-10-28 18:09:06 +0100

    New method toString

--- src/main/java/at/ac/uibk/dps/biohadoop/utils/PropertyConverter.java
+++ src/main/java/at/ac/uibk/dps/biohadoop/utils/PropertyConverter.java
@@ -1,5 +1,6 @@
 package at.ac.uibk.dps.biohadoop.utils;
 
+import java.util.Arrays;
 import java.util.Map;
 
 import at.ac.uibk.dps.biohadoop.algorithm.AlgorithmException;
@@ -17,7 +18,7 @@ public class PropertyConverter {
 					+ " to int, value was " + value, e);
 		}
 	}
-	
+
 	public static long toLong(Map<String, String> properties, String key)
 			throws AlgorithmException {
 		String value = null;
@@ -28,5 +29,28 @@ public class PropertyConverter {
 			throw new AlgorithmException("Could not convert property " + key
 					+ " to long, value was " + value, e);
 		}
+	}
+
+	public static String toString(Map<String, String> properties, String key,
+			String[] validVals) throws AlgorithmException {
+		String value = properties.get(key);
+		if (validVals == null) {
+			return value;
+		}
+		for (String validVal : validVals) {
+			if (validVal == null) {
+				if (value == null) {
+					return value;
+				}
+			} else {
+				if (validVal.equals(value)) {
+					return value;
+				}
+			}
+		}
+
+		throw new AlgorithmException("Property " + key
+				+ " has invalid value. Valid values are: "
+				+ Arrays.toString(validVals));
 	}
 }
