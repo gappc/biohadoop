72780a786635baa670123671c95273a90d27ab78
Christian Gapp <gapp.christian@gmail.com>
2014-11-10 01:49:17 +0100

    Adjusting deprecated Hadoop method call

--- src/main/java/at/ac/uibk/dps/biohadoop/hadoop/BiohadoopClient.java
+++ src/main/java/at/ac/uibk/dps/biohadoop/hadoop/BiohadoopClient.java
@@ -213,12 +213,12 @@ public class BiohadoopClient {
 	private void setupAppMasterEnv(Map<String, String> appMasterEnv,
 			Configuration conf) {
 		Apps.addToEnvironment(appMasterEnv, Environment.CLASSPATH.name(),
-				Environment.PWD.$() + File.separator + "*");
+				Environment.PWD.$() + File.separator + "*", File.pathSeparator);
 		for (String c : conf.getStrings(
 				YarnConfiguration.YARN_APPLICATION_CLASSPATH,
 				YarnConfiguration.DEFAULT_YARN_APPLICATION_CLASSPATH)) {
 			Apps.addToEnvironment(appMasterEnv, Environment.CLASSPATH.name(),
-					c.trim());
+					c.trim(), File.pathSeparator);
 		}
 	}
 
