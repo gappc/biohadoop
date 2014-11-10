02cdef892e533ecd50c0f07ecc8ac07c41162892
Christian Gapp <gapp.christian@gmail.com>
2014-11-10 01:48:49 +0100

    Getters added

--- src/main/java/at/ac/uibk/dps/biohadoop/datastore/Option.java
+++ src/main/java/at/ac/uibk/dps/biohadoop/datastore/Option.java
@@ -1,27 +1,10 @@
 package at.ac.uibk.dps.biohadoop.datastore;
 
-import java.io.InvalidObjectException;
-import java.io.ObjectStreamException;
 import java.io.Serializable;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Option<T> implements Serializable {
 
-	private static final long serialVersionUID = -1564427329140182760L;
-
-	private static final Logger LOG = LoggerFactory.getLogger(Option.class);
+	private static final long serialVersionUID = -3765321042287092766L;
 
 	private final String name;
 	private final Class<T> clazz;
@@ -30,4 +13,13 @@ public class Option<T> implements Serializable {
 		this.name = name;
 		this.clazz = clazz;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Class<T> getClazz() {
+		return clazz;
+	}
+	
 }
