0e5f4ea1d62678e3ca3c25427b3bff5a55b62469
Christian Gapp <gapp.christian@gmail.com>
2014-11-10 01:40:58 +0100

    Removed unused log variable

--- src/main/java/at/ac/uibk/dps/biohadoop/persistence/FileSaver.java
+++ src/main/java/at/ac/uibk/dps/biohadoop/persistence/FileSaver.java
@@ -1,25 +1,14 @@
 package at.ac.uibk.dps.biohadoop.persistence;
 
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Map;
-
-import org.apache.hadoop.yarn.conf.YarnConfiguration;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import at.ac.uibk.dps.biohadoop.solver.SolverData;
 import at.ac.uibk.dps.biohadoop.solver.SolverId;
-import at.ac.uibk.dps.biohadoop.utils.HdfsUtil;
 
 public class FileSaver {
 
 	public static final String FILE_SAVE_PATH = "FILE_SAVE_PATH";
 	public static final String FILE_SAVE_AFTER_ITERATION = "FILE_SAVE_AFTER_ITERATION";
-
-	private static final Logger LOG = LoggerFactory.getLogger(FileSaver.class);
 
 	public static void saveRollingJson(SolverId solverId, Map<String, String> properties,
 			SolverData<?> solverData) throws FileSaveException {
